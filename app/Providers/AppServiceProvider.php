1aecdbdbed8c85e1f91c84ab71540e52ccfc0507
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-05-13 11:44:04 -0700

    feat: add strict pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -19,6 +27,45 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        self::configureModels();
+        self::configureVite();
+        self::configureSchema();
+        self::configureActions();
+        self::configureCommands();
+        self::configureDates();
+    }
+
+    private function configureModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    private function configureSchema(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    private function configureActions(): void
+    {
         //
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
